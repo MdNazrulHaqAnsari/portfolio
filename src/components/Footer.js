1e8e8b852161ec4b722f61e9deb76d158eb66862
Nazrul090831 <nazrul090831@gmail.com>
2025-01-18 23:26:42 +0530

    minor changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,9 @@
 export default function Footer() {
   return (
     <footer className="flex flex-col items-center w-full max-w-4xl row-start-3 p-0 text-sm text-center sm:p-5 sm:flex-row sm:justify-between sm:text-base">
-      {/* Left Content */}
       <p className="text-gray-600">
         &copy; {new Date().getFullYear()} | Mohammed Nazrul Haq Ansari
       </p>
-
-      {/* Right Content */}
       <a
         href="https://linkedin.com/in/mohammed-nazrul-haq-ansari"
         target="_blank"
@@ -14,7 +11,7 @@ export default function Footer() {
         className="flex items-center gap-2 mb-2 text-blue-600 sm:mb-0 hover:underline"
       >
         <Image
-          src="./linkedin.svg" // Replace with your LinkedIn icon path
+          src="./linkedin.svg"
           alt="LinkedIn"
           width={28.8}
           height={28.8}
